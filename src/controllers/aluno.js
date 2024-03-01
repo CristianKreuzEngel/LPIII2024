719922c28f7d626a3bc4424f4492fb66f5286085
Cristian Engel <kreuzengelc@gmail.com>
2024-02-29 21:33:59 -0300

    configurando rota e controllers

--- src/controllers/aluno.js
+++ src/controllers/aluno.js
@@ -8,5 +8,14 @@ const getAluno = async(req, res, next) => {
         res.status(500).send(err)
     }
 }
+const postAluno = async(req, res, next) => {
+    try{
+        const retorno = await alunoService.getAluno(req.params)
+        res.status(201).json(retorno)
+    }catch(err){
+        res.status(500).send(err)
+    }
+}
 
 module.exports.getAluno = getAluno
+module.exports.postAluno = postAluno
--- src/routes/aluno.js
+++ src/routes/aluno.js
@@ -2,4 +2,5 @@ const alunoController = require('../controllers/aluno');
 
 module.exports = (app) =>{
     app.get('/aluno/:name', alunoController.getAluno)
+    app.post('/aluno', alunoController.postAluno)
 }
