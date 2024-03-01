5824b30cff5939c079f400eba771372b70127719
Cristian Engel <kreuzengelc@gmail.com>
2024-02-29 21:23:58 -0300

    procedimento postAluno

--- src/services/aluno.js
+++ src/services/aluno.js
@@ -1,3 +1,14 @@
+const db = require('../configs/pg');
+
+const sql = 
+    `insert into alunos(id, nome, sobrenome, periodo, observacao)
+                        values ($1, $2, $3, $4, $5)`
+
+const postAluno = async (params) => {
+    const {id, nome, sobrenome, periodo, observacao } = params
+    await db.query(sql, [id, nome, sobrenome, periodo, observacao])
+}
+
 const getAluno = async (params) => {
     let aluno = {}
     if(params.name === "Cristian") {
@@ -13,4 +24,5 @@ const getAluno = async (params) => {
     return aluno
 }
 
+module.exports.postAluno = postAluno;
 module.exports.getAluno = getAluno;
