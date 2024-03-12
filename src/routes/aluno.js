7e9f6ae702629868da51c717646a2f340dbe34cb
Cristian Engel <kreuzengelc@gmail.com>
2024-03-11 21:24:22 -0300

    finalização do crud

--- src/routes/aluno.js
+++ src/routes/aluno.js
@@ -1,6 +1,9 @@
 const alunoController = require('../controllers/aluno');
 
 module.exports = (app) =>{
-    app.get('/aluno/:name', alunoController.getAluno)
+    app.get('/aluno', alunoController.getAluno)
     app.post('/aluno', alunoController.postAluno)
+    app.delete('/aluno/:id', alunoController.deleteAluno)
+    app.put('/aluno/:id', alunoController.putAluno)
+    app.patch('/aluno/:id', alunoController.patchAluno)
 }
--- src/services/aluno.js
+++ src/services/aluno.js
@@ -1,28 +1,86 @@
 const db = require('../configs/pg');
 
-const sql = 
+const sql_insert = 
     `insert into alunos(id, nome, sobrenome, periodo, observacao)
                         values ($1, $2, $3, $4, $5)`
 
 const postAluno = async (params) => {
     const {id, nome, sobrenome, periodo, observacao } = params
-    await db.query(sql, [id, nome, sobrenome, periodo, observacao])
+    return await db.query(sql, [id, nome, sobrenome, periodo, observacao])
 }
+const sql_get =`select id,
+                       nome,
+                       sobrenome,
+                       periodo,
+                       observacao
+                    from alunos`
 
-const getAluno = async (params) => {
+const getAluno = async () => {
     let aluno = {}
-    if(params.name === "Cristian") {
-        aluno.id = 19283
-        aluno.nome = "Cristian"
-        aluno.sobrenome = "Kreuz Engel"
-        aluno.notas = []
-        aluno.notas.push(10)
-        aluno.notas.push(9)
-    }else{
-        throw "Aluno não cadastrado!"
-    }
+    await db.query(sql_get)
+            .then(ret => aluno ={total: ret.rows.length, alunos: ret.rows })
+            .catch(err => aluno = err.stack )
     return aluno
 }
 
+const sql_delete = 
+    `delete from alunos
+     where id = $1`
+
+const deleteAluno = async (params) => {
+    const {id} = params
+    await db.query(sql_delete, [id])
+}
+
+const sql_put = 
+    `update alunos
+        set nome = $2,
+            sobrenome = $3,
+            periodo = $4,
+            observacao = $5
+     where id = $1`
+
+const putAluno = async (params) => {
+    const {id, nome, sobrenome, periodo, observacao} = params
+    return await db.query(sql_put, [id, nome, sobrenome, periodo, observacao])
+}
+
+
+const sql_patch =
+        `update alunos
+            set`
+
+const patchAluno = async (params) => {
+    let fields = ''
+    let binds = []
+    binds.push(params.id)
+    let countParams = 1
+    if(params.nome) {
+        countParams ++
+        fields += `nome = $${countParams}`
+        binds.push(params.nome)
+    }
+    if (params.sobrenome) {
+        countParams ++
+        fields += (fields?',':'') + ` sobrenome = $${countParams} `
+        binds.push(params.sobrenome)
+      }
+      if (params.periodo) {
+        countParams ++
+        fields += (fields?',':'') + ` periodo = $${countParams} `
+        binds.push(params.periodo)
+      }
+      if (params.observacao) {
+        countParams ++
+        fields += (fields?',':'') + ` observacao = $${countParams} `
+        binds.push(params.observacao)
+      }
+      let sql = sql_patch + fields + ' where id = $1 '
+      return await db.query(sql, binds)
+}    
+       
 module.exports.postAluno = postAluno;
 module.exports.getAluno = getAluno;
+module.exports.deleteAluno = deleteAluno
+module.exports.putAluno = putAluno
+module.exports.patchAluno = patchAluno
